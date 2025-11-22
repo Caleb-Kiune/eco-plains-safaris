99be1babc7bbf2646e3b1cb7bac141a9a526f520
Caleb <calebkiune@gmail.com>
2025-11-22 12:05:20 +0300

    feat(ui): improve section contrast between home components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,14 +5,20 @@ import AboutSection from '../components/home/AboutSection';
 import FeaturedDestinations from '../components/home/FeaturedDestinations';
 import TripsCarousel from '../components/home/TripsCarousel';
 import WhyChooseUs from '../components/home/WhyChooseUs';
+import SectionDivider from '../components/common/SectionDivider';
 
 export default function HomePage() {
   return (
     <>
+     
       <HeroSection />
+      <SectionDivider/>
       <AboutSection />
+      <SectionDivider/>
       <FeaturedDestinations />
+      <SectionDivider/>
       <TripsCarousel />
+      <SectionDivider/>
       <WhyChooseUs />
     </>
   );
